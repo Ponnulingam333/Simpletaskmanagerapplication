546f1405c58d9ae2c086f37fe13ed3a3ca91df0d
Ponnulingam <168225396+Ponnulingam333@users.noreply.github.com>
2025-12-29 23:41:29 +0530

    Update TaskManagerApplication.java

--- TaskManagerApplication.java
+++ TaskManagerApplication.java
@@ -6,19 +6,25 @@ public static void main(String args[]) {
 	Scanner scan =new Scanner(System.in);
 	int useropt;
 	Taskopt topt =new Taskopt();
-	try{while(true) {
+          while(true) 
+try{
 		topt.showtask();
 		System.out.println("Enter your choice (1-6)");
 		useropt =scan.nextInt();
 		if(useropt==6) {
 			System.out.println("Thank you");
 			break;}
-		topt.inputs(useropt);}}
-		
-	catch(InputMismatchException i) {
+		topt.inputs(useropt);
+}
+catch(InputMismatchException i) {
+                 scan.nextLine();
 			System.out.println("Invalid choice. Please enter a number from 1 to 6");
-			TaskManagerApplication.main(null);
-			} scan.close();
+                        System.out.println("Press enter to continue");
+                      scan.nextLine();
+			} 
+}
+		
+	scan.close();
 	
 }
 }
